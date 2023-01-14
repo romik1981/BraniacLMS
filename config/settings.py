9acea72ec8074d29b6fe459f47ea2d04ca08e013
Belyakov Roman <belyakov1981@mail.ru>
2023-01-14 18:11:31 +0300

    lesson_4 Fixtures

--- config/settings.py
+++ config/settings.py
@@ -77,14 +77,21 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "db_braniaclms",
+#         "USER": "romik1981",
+#         "PASSWORD": "Denis-1103",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "db_braniaclms",
-        "USER": "romik1981",
-        "PASSWORD": "Denis-1103",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "db.sqlite3",
     }
 }
 
